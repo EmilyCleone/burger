f33753ba00b0df475b05a4fe2be4ec26d7646af7
Emily Johnson-Eaton <emilyjohnsoneaton@gmail.com>
2019-10-14 16:21:06 -0600

    added connection.js with code to connect to mySQL database. Also added config folder in burger app

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', 0);
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom & Swiss Burger', 0);
+INSERT INTO burgers (name, devoured) VALUES ('Peanutbutter Burger', 0);
+INSERT INTO burgers (name, devoured) VALUES ('Black Bean Burger', 0);
+INSERT INTO burgers (name, devoured) VALUES ('Sweet Potato Burger', 0);
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom & Garlic Burger', 0);
